9af2507c806927ea0446ff260103164bc08037d0
Chinweuba Azunna <doneymore@gmail.com>
2025-09-17 06:27:14 +0100

    temporal routing to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
+import LandingPage from "./pages/landingScreen/page";
+
 export default function Home() {
   return (
-    <div className="bg-shades-azul_bright font-orelega">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="h-80 w-96 text-center text-primary-50 ">
-          Hello world
-        </div>
-      </main>
+    <div className="">
+      <LandingPage />
     </div>
   );
 }
